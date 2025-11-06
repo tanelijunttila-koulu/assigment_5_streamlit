2ecca9d41e42067498a4b3ca38ef5a9cad178cae
Taneli Junttila <152056764+TaneliJunttila@users.noreply.github.com>
2025-11-06 15:53:00 +0200

    Class changes

--- app.py
+++ app.py
@@ -17,6 +17,27 @@ st.dataframe(vuosi_df)
 #st.line_chart(vuosi_df, x="Vuosi", y="Huonekäyttöaste, % Rovaniemi")
 st.bar_chart(vuosi_df)
 
-st.dataframe(rovaniemi_df)
-
 # This is test
+
+# ulk vs kot
+rovaniemi_df["Kotimaiset yöpymiset, lkm Rovaniemi"] = pd.to_numeric(rovaniemi_df["Kotimaiset yöpymiset, lkm Rovaniemi"])
+rovaniemi_df["Ulkomaiset yöpymiset Rovaniemi"] = rovaniemi_df["Ulkomaiset yöpymiset Rovaniemi"].str.replace(".", "")
+rovaniemi_df["Ulkomaiset yöpymiset Rovaniemi"] = pd.to_numeric(rovaniemi_df["Ulkomaiset yöpymiset Rovaniemi"])
+ulko_vs_kot_df = rovaniemi_df[["Vuosi", "Kotimaiset yöpymiset, lkm Rovaniemi", "Ulkomaiset yöpymiset Rovaniemi"]]
+ulko_vs_kot_year_df = ulko_vs_kot_df.groupby(by="Vuosi").sum()
+st.dataframe(ulko_vs_kot_year_df)
+st.line_chart(ulko_vs_kot_year_df)
+st.text("Hello World")
+
+area_df = rovaniemi_df[["Vuosi", "RevPar, EUR Rovaniemi", "Huoneen keskihinta Rovaniemi"]]
+
+area_df["RevPar, EUR Rovaniemi"] = area_df["RevPar, EUR Rovaniemi"].str.replace(".", "")
+area_df["RevPar, EUR Rovaniemi"] = pd.to_numeric(area_df["RevPar, EUR Rovaniemi"])
+
+area_df["Huoneen keskihinta Rovaniemi"] = area_df["Huoneen keskihinta Rovaniemi"].str.replace(".", "")
+area_df["Huoneen keskihinta Rovaniemi"] = pd.to_numeric(area_df["Huoneen keskihinta Rovaniemi"])
+
+area_df = area_df.groupby(by="Vuosi").sum()
+st.area_chart(area_df, y=["RevPar, EUR Rovaniemi", "Huoneen keskihinta Rovaniemi"])
+
+# st.area_chart
